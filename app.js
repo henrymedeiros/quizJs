767290d96b5ea73498442926f6782434f824cfd4
Henry Medeiros <henrymedeiros77@gmail.com>
2021-02-01 22:06:08 -0300

    Adicionando frase

--- app.js
+++ app.js
@@ -18,8 +18,29 @@ form.addEventListener('submit', e => {
     // show result
     console.log(score);
     result.querySelector('span').textContent = `${score}%`;
+    // insert phrase
+    if(score===25){
+        result.querySelector('p').innerHTML += `Estude um pouco mais de teoria.`
+    }
+    else if(score===50){
+        result.querySelector('p').innerHTML += `Você ainda pode melhorar!`
+    }
+    else if(score===75){
+        result.querySelector('p').innerHTML += `Esta quase lá!`
+    }
+    else if(score===100){
+        result.querySelector('p').innerHTML += `Você está em dia com os estudos, parabéns!`
+    }
+    else if(score===0){
+        result.querySelector('p').innerHTML += `Revise os conteúdos e tente novamente.`
+    }
     result.classList.remove("d-none");
+    
     window.scrollTo(0, 0);
+
+    
+
+
     
 })
 
